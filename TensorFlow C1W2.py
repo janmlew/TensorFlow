85db30d58b4f5ee9755637001ff837b2e84523bd
janmlew <janmikolajlewinski@gmail.com>
2023-08-22 13:17:20 +0200

    Basic model functionality. Loaded MNIST images and initialized the model.

--- TensorFlow C1W2.py
+++ TensorFlow C1W2.py
@@ -7,12 +7,23 @@ Course 1., week 2.
 """
 
 # Imports:
-import numpy as np
 import tensorflow as tf
-from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 # Data loads:
 
 
+# Load the Fashion MNIST dataset
+fmnist = tf.keras.datasets.fashion_mnist
+# Load the training and test split of the Fashion MNIST dataset
+(training_images, training_labels), (test_images, test_labels) = fmnist.load_data()
+
+
 # Main code:
+# Build the classification model
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
