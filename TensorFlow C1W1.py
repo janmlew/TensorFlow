efaa65ef63431db090c46a7503a4894e3cd0381f
janmlew <janmikolajlewinski@gmail.com>
2023-08-22 10:49:48 +0200

    Changed filename, initial imports.

---
+++ TensorFlow C1W1.py
@@ -0,0 +1,16 @@
+"""
+
+My project on DeepLearning.AI TensorFlow Developer Professional Certificate.
+
+Main project file.
+
+"""
+
+# Imports:
+import numpy as np
+import tensorflow as tf
+
+# Data loads:
+
+# Main code:
+
