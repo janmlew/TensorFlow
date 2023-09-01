0e258377ce779d097c0f298c4925fe1004b0b6ff
janmlew <janmikolajlewinski@gmail.com>
2023-09-01 16:29:35 +0200

    Added visualizations for the filtering.

--- TensorFlow C1W3.py
+++ TensorFlow C1W3.py
@@ -8,6 +8,8 @@ Course 1., week 3.
 
 # Imports:
 import tensorflow as tf
+import matplotlib.pyplot as plt
+# from tensorflow.keras import models
 
 # Load the Fashion MNIST dataset
 fmnist = tf.keras.datasets.fashion_mnist
@@ -96,3 +98,27 @@ print(f'\nCONVOLUTIONAL MODEL EVALUATION:')
 test_loss = model.evaluate(test_images, test_labels)
 print(test_loss)
 
+print(test_labels[:100])
+
+f, axarr = plt.subplots(3, 4)
+
+FIRST_IMAGE = 0
+SECOND_IMAGE = 23
+THIRD_IMAGE = 28
+CONVOLUTION_NUMBER = 1
+
+layer_outputs = [layer.output for layer in model.layers]
+activation_model = tf.keras.models.Model(inputs=model.input, outputs=layer_outputs)
+
+for x in range(0, 4):
+    f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
+    axarr[0, x].imshow(f1[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
+    axarr[0, x].grid(False)
+
+    f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
+    axarr[1, x].imshow(f2[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
+    axarr[1, x].grid(False)
+
+    f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
+    axarr[2, x].imshow(f3[0, :, :, CONVOLUTION_NUMBER], cmap='inferno')
+    axarr[2, x].grid(False)
