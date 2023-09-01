dc044b951f27ee468c997ab60bb1dc3b52f47993
janmlew <janmikolajlewinski@gmail.com>
2023-09-01 09:22:44 +0200

    Coded the convolutional model.

--- TensorFlow C1W3.py
+++ TensorFlow C1W3.py
@@ -8,3 +8,60 @@ Course 1., week 3.
 
 # Imports:
 import tensorflow as tf
+
+# Load the Fashion MNIST dataset
+fmnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = fmnist.load_data()
+
+# Normalize the pixel values
+training_images = training_images / 255.0
+test_images = test_images / 255.0
+
+# Define the *shallow* model
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+# Setup training parameters
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+print(f'\nSHALLOW MODEL TRAINING:')
+model.fit(training_images, training_labels, epochs=5)
+
+# Evaluate on the test set
+print(f'\nSHALLOW MODEL EVALUATION:')
+test_loss = model.evaluate(test_images, test_labels)
+print(test_loss)
+
+# Define the *convolutional* model
+model = tf.keras.models.Sequential([
+
+    # Add convolutions and max pooling
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    # Add the same layers as before
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# Print the model summary
+model.summary()
+
+# Use same settings
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+print(f'\nCONVOLUTIONAL MODEL TRAINING:')
+model.fit(training_images, training_labels, epochs=5)
+
+# Evaluate on the test set
+print(f'\nCONVOLUTIONAL MODEL EVALUATION:')
+test_loss = model.evaluate(test_images, test_labels)
+print(test_loss)
