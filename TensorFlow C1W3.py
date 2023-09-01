f3bfa22b2da9faf0d77dcdddcff5340393d9fe2b
janmlew <janmikolajlewinski@gmail.com>
2023-09-01 16:10:11 +0200

    Added second convolutional model with expanded filters.

--- TensorFlow C1W3.py
+++ TensorFlow C1W3.py
@@ -65,3 +65,34 @@ model.fit(training_images, training_labels, epochs=5)
 print(f'\nCONVOLUTIONAL MODEL EVALUATION:')
 test_loss = model.evaluate(test_images, test_labels)
 print(test_loss)
+
+# Define the *convolutional* model
+model = tf.keras.models.Sequential([
+
+    # Add convolutions and max pooling
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    # Add the same layers as before
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# Print the model summary
+model.summary()
+
+# Use same settings
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+print(f'\nCONVOLUTIONAL MODEL TRAINING:')
+model.fit(training_images, training_labels, epochs=5)
+
+# Evaluate on the test set
+print(f'\nCONVOLUTIONAL MODEL EVALUATION:')
+test_loss = model.evaluate(test_images, test_labels)
+print(test_loss)
+
