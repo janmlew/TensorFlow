67a555d2daf7c9a94e702999ea07266e0ba67c97
janmlew <janmikolajlewinski@gmail.com>
2023-09-03 11:40:14 +0200

    Added some code, mainly callbacks. Renamed old files for consistency and added a new file.

--- C1W3 1st exercises.py
+++ C1W3 1st exercises.py
@@ -105,7 +105,6 @@ print(f'\nMODEL EVALUATION:')
 test_loss = model.evaluate(test_images, test_labels)
 print(test_loss)
 
-
 '''
 2. Remove the final Convolution. What impact will this have on accuracy or training time?
 '''
@@ -117,7 +116,7 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
     tf.keras.layers.MaxPooling2D(2, 2),
     #    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2, 2),
+    #    tf.keras.layers.MaxPooling2D(2, 2),
 
     # Add the same layers as before
     tf.keras.layers.Flatten(),
@@ -140,3 +139,73 @@ print(test_loss)
 """
 3. How about adding more Convolutions? What impact do you think this will have? Experiment with it.
 """
+
+# Define the model
+model = tf.keras.models.Sequential([
+
+    # Add convolutions and max pooling
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    # Add the same layers as before
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# Use same settings
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+print(f'\nMODEL TRAINING:')
+model.fit(training_images, training_labels, epochs=5)
+
+# Evaluate on the test set
+print(f'\nMODEL EVALUATION:')
+test_loss = model.evaluate(test_images, test_labels)
+print(test_loss)
+
+"""
+4. In the previous lesson you implemented a callback to check on the loss function and to cancel training once it hit a 
+certain amount. See if you can implement that here.
+"""
+
+class MyCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('accuracy') >= 0.6:
+            print("\nReached 60% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+
+callbacks = MyCallback()
+
+# Define the model
+model = tf.keras.models.Sequential([
+
+    # Add convolutions and max pooling
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    # Add the same layers as before
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# Use same settings
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+print(f'\nMODEL TRAINING:')
+model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
+
+# Evaluate on the test set
+print(f'\nMODEL EVALUATION:')
+test_loss = model.evaluate(test_images, test_labels)
+print(test_loss)
