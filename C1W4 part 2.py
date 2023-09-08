b1b0cb2994a6aaf261956b6264541f6974d01baa
janmlew <janmikolajlewinski@gmail.com>
2023-09-08 11:36:54 +0200

    Code + resulting validation files.

---
+++ C1W4 part 2.py
@@ -0,0 +1,218 @@
+import zipfile
+import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# import numpy as np
+# from google.colab import files
+from tensorflow.keras.utils import load_img, img_to_array
+import numpy as np
+import random
+
+# Unzip training set
+local_zip = './horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('./horse-or-human')
+
+# Unzip validation set
+local_zip = './validation-horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('./validation-horse-or-human')
+
+zip_ref.close()
+
+# Directory with training horse pictures
+train_horse_dir = os.path.join('./horse-or-human/horses')
+
+# Directory with training human pictures
+train_human_dir = os.path.join('./horse-or-human/humans')
+
+# Directory with validation horse pictures
+validation_horse_dir = os.path.join('./validation-horse-or-human/horses')
+
+# Directory with validation human pictures
+validation_human_dir = os.path.join('./validation-horse-or-human/humans')
+
+train_horse_names = os.listdir(train_horse_dir)
+print(f'TRAIN SET HORSES: {train_horse_names[:10]}')
+
+train_human_names = os.listdir(train_human_dir)
+print(f'TRAIN SET HUMANS: {train_human_names[:10]}')
+
+validation_horse_names = os.listdir(validation_horse_dir)
+print(f'VAL SET HORSES: {validation_horse_names[:10]}')
+
+validation_human_names = os.listdir(validation_human_dir)
+print(f'VAL SET HUMANS: {validation_human_names[:10]}')
+
+print(f'total training horse images: {len(os.listdir(train_horse_dir))}')
+print(f'total training human images: {len(os.listdir(train_human_dir))}')
+print(f'total validation horse images: {len(os.listdir(validation_horse_dir))}')
+print(f'total validation human images: {len(os.listdir(validation_human_dir))}')
+
+# %matplotlib inline
+
+# Parameters for our graph; we'll output images in a 4x4 configuration
+nrows = 4
+ncols = 4
+
+# Index for iterating over images
+pic_index = 0
+
+# Set up matplotlib fig, and size it to fit 4x4 pics
+fig = plt.gcf()
+fig.set_size_inches(ncols * 4, nrows * 4)
+
+pic_index += 8
+next_horse_pix = [os.path.join(train_horse_dir, fname)
+                  for fname in train_horse_names[pic_index - 8:pic_index]]
+next_human_pix = [os.path.join(train_human_dir, fname)
+                  for fname in train_human_names[pic_index - 8:pic_index]]
+
+for i, img_path in enumerate(next_horse_pix + next_human_pix):
+    # Set up subplot; subplot indices start at 1
+    sp = plt.subplot(nrows, ncols, i + 1)
+    sp.axis('Off')  # Don't show axes (or gridlines)
+
+    img = mpimg.imread(img_path)
+    plt.imshow(img)
+
+plt.show()
+
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 300x300 with 3 bytes color
+    # This is the first convolution
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(300, 300, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('horses') and 1 for the other (
+    # 'humans')
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(learning_rate=0.001),
+              metrics=['accuracy'])
+
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1 / 255)
+validation_datagen = ImageDataGenerator(rescale=1 / 255)
+
+# Flow training images in batches of 128 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+    './horse-or-human/',  # This is the source directory for training images
+    target_size=(300, 300),  # All images will be resized to 300x300
+    batch_size=128,
+    # Since you use binary_crossentropy loss, you need binary labels
+    class_mode='binary')
+
+# Flow validation images in batches of 128 using validation_datagen generator
+validation_generator = validation_datagen.flow_from_directory(
+    './validation-horse-or-human/',  # This is the source directory for validation images
+    target_size=(300, 300),  # All images will be resized to 300x300
+    batch_size=32,
+    # Since you use binary_crossentropy loss, you need binary labels
+    class_mode='binary')
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch=8,
+    epochs=15,
+    verbose=1,
+    validation_data=validation_generator,
+    validation_steps=8)
+
+"""
+uploaded = files.upload()
+
+for fn in uploaded.keys():
+
+    # predicting images
+    path = '/content/' + fn
+    img = load_img(path, target_size=(300, 300))
+    x = img_to_array(img)
+    x /= 255
+    x = np.expand_dims(x, axis=0)
+
+    images = np.vstack([x])
+    classes = model.predict(images, batch_size=10)
+    print(classes[0])
+    if classes[0] > 0.5:
+        print(fn + " is a human")
+    else:
+        print(fn + " is a horse")
+"""
+
+# Define a new Model that will take an image as input, and will output
+# intermediate representations for all layers in the previous model after
+# the first.
+successive_outputs = [layer.output for layer in model.layers[1:]]
+visualization_model = tf.keras.models.Model(inputs=model.input, outputs=successive_outputs)
+
+# Prepare a random input image from the training set.
+horse_img_files = [os.path.join(train_horse_dir, f) for f in train_horse_names]
+human_img_files = [os.path.join(train_human_dir, f) for f in train_human_names]
+img_path = random.choice(horse_img_files + human_img_files)
+
+img = load_img(img_path, target_size=(300, 300))  # this is a PIL image
+x = img_to_array(img)  # Numpy array with shape (300, 300, 3)
+x = x.reshape((1,) + x.shape)  # Numpy array with shape (1, 300, 300, 3)
+
+# Scale by 1/255
+x /= 255
+
+# Run the image through the network, thus obtaining all
+# intermediate representations for this image.
+successive_feature_maps = visualization_model.predict(x)
+
+# These are the names of the layers, so you can have them as part of the plot
+layer_names = [layer.name for layer in model.layers[1:]]
+
+# Display the representations
+for layer_name, feature_map in zip(layer_names, successive_feature_maps):
+    if len(feature_map.shape) == 4:
+
+        # Just do this for the conv / maxpool layers, not the fully-connected layers
+        n_features = feature_map.shape[-1]  # number of features in feature map
+
+        # The feature map has shape (1, size, size, n_features)
+        size = feature_map.shape[1]
+
+        # Tile the images in this matrix
+        display_grid = np.zeros((size, size * n_features))
+        for i in range(n_features):
+            x = feature_map[0, :, :, i]
+            x -= x.mean()
+            x /= x.std()
+            x *= 64
+            x += 128
+            x = np.clip(x, 0, 255).astype('uint8')
+
+            # Tile each filter into this big horizontal grid
+            display_grid[:, i * size: (i + 1) * size] = x
+
+        # Display the grid
+        scale = 20. / n_features
+        plt.figure(figsize=(scale * n_features, scale))
+        plt.title(layer_name)
+        plt.grid(False)
+        plt.imshow(display_grid, aspect='auto', cmap='viridis')
