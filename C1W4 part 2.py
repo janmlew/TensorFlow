509e96777136d600e6322adb6f8049ef89ebd81d
janmlew <janmikolajlewinski@gmail.com>
2023-09-09 08:59:45 +0200

    Added ignored Colab code for consistency.

--- C1W4 part 2.py
+++ C1W4 part 2.py
@@ -140,6 +140,27 @@ history = model.fit(
     validation_data=validation_generator,
     validation_steps=8)
 
+"""
+uploaded = files.upload()
+
+for fn in uploaded.keys():
+
+    # predicting images
+    path = '/content/' + fn
+    img = load_img(path, target_size=(300, 300))
+    x = img_to_array(img)
+    x /= 255
+    x = np.expand_dims(x, axis=0)
+
+    images = np.vstack([x])
+    classes = model.predict(images, batch_size=10)
+    print(classes[0])
+    if classes[0] > 0.5:
+        print(fn + " is a human")
+    else:
+        print(fn + " is a horse")
+"""
+
 # Define a new Model that will take an image as input, and will output
 # intermediate representations for all layers in the previous model after
 # the first.
