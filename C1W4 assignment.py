88b28ccf00c123b14566b40818243db30420029b
janmlew <janmikolajlewinski@gmail.com>
2023-09-10 08:47:27 +0200

    Code + structural fixes.

---
+++ C1W4 assignment.py
@@ -0,0 +1,109 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import os
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
+from tensorflow.keras import optimizers, losses
+# import base64
+
+base_dir = "./data/"
+happy_dir = os.path.join(base_dir, "happy/")
+sad_dir = os.path.join(base_dir, "sad/")
+
+print("Sample happy image:")
+plt.imshow(load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}"))
+plt.show()
+
+print("\nSample sad image:")
+plt.imshow(load_img(f"{os.path.join(sad_dir, os.listdir(sad_dir)[0])}"))
+plt.show()
+
+# Load the first example of a happy face
+sample_image  = load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}")
+
+# Convert the image into its numpy array representation
+sample_array = img_to_array(sample_image)
+
+print(f"Each image has shape: {sample_array.shape}")
+
+print(f"The maximum pixel value used is: {np.max(sample_array)}")
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('accuracy') is not None and logs.get('accuracy') > 0.999:
+            print("\nReached 99.9% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+# GRADED FUNCTION: image_generator
+def image_generator():
+    ### START CODE HERE
+
+    # Instantiate the ImageDataGenerator class.
+    # Remember to set the rescale argument.
+    train_datagen = None
+
+    # Specify the method to load images from a directory and pass in the appropriate arguments:
+    # - directory: should be a relative path to the directory containing the data
+    # - targe_size: set this equal to the resolution of each image (excluding the color dimension)
+    # - batch_size: number of images the generator yields when asked for a next batch. Set this to 10.
+    # - class_mode: How the labels are represented. Should be one of "binary", "categorical" or "sparse".
+    #               Pick the one that better suits here given that the labels are going to be 1D binary labels.
+    train_generator = train_datagen.flow_from_directory(directory=None,
+                                                        target_size=(None, None),
+                                                        batch_size=None,
+                                                        class_mode=None)
+    ### END CODE HERE
+
+    return train_generator
+
+# Save your generator in a variable
+gen = image_generator()
+
+# GRADED FUNCTION: train_happy_sad_model
+def train_happy_sad_model(train_generator):
+    # Instantiate the callback
+    callbacks = myCallback()
+
+    ### START CODE HERE
+
+    # Define the model
+    model = tf.keras.models.Sequential([
+        None,
+    ])
+
+    # Compile the model
+    # Select a loss function compatible with the last layer of your network
+    model.compile(loss=losses.
+    None,
+    optimizer = optimizers.
+    None,
+    metrics = ['accuracy'])
+
+
+
+    # Train the model
+    # Your model should achieve the desired accuracy in less than 15 epochs.
+    # You can hardcode up to 20 epochs in the function below but the callback should trigger before 15.
+    history = model.fit(x=None,
+                        epochs=None,
+                        callbacks=[None]
+                        )
+
+    ### END CODE HERE
+    return history
+
+hist = train_happy_sad_model(gen)
+
+print(f"Your model reached the desired accuracy after {len(hist.epoch)} epochs")
+
+if not "accuracy" in hist.model.metrics_names:
+    print("Use 'accuracy' as metric when compiling your model.")
+else:
+    print("The metric was correctly defined.")
+
+# encoded_answer = "ClNvbWUgaGVscGZ1bCB0aXBzIGluIGNhc2UgeW91IGFyZSBzdHVjazoKCiAgICAtIEEgZ29vZCBmaXJzdCBsYXllciB3b3VsZCBiZSBhIENvbnYyRCBsYXllciB3aXRoIGFuIGlucHV0IHNoYXBlIHRoYXQgbWF0Y2hlcyAKICAgIHRoYXQgb2YgZXZlcnkgaW1hZ2UgaW4gdGhlIHRyYWluaW5nIHNldCAoaW5jbHVkaW5nIHRoZSBjb2xvciBkaW1lbnNpb24pCiAgICAKICAgIC0gVGhlIG1vZGVsIHdpbGwgd29yayBiZXN0IHdpdGggMyBjb252b2x1dGlvbmFsIGxheWVycwogICAgCiAgICAtIFRoZXJlIHNob3VsZCBiZSBhIEZsYXR0ZW4gbGF5ZXIgaW4gYmV0d2VlbiBjb252b2x1dGlvbmFsIGFuZCBkZW5zZSBsYXllcnMKICAgIAogICAgLSBUaGUgZmluYWwgbGF5ZXIgc2hvdWxkIGJlIGEgRGVuc2UgbGF5ZXIgd2l0aCB0aGUgbnVtYmVyIG9mIHVuaXRzIGFuZCAKICAgIGFjdGl2YXRpb24gZnVuY3Rpb24gdGhhdCBzdXBwb3J0cyBiaW5hcnkgY2xhc3NpZmljYXRpb24uCg=="
+# encoded_answer = encoded_answer.encode('ascii')
+# answer = base64.b64decode(encoded_answer)
+# answer = answer.decode('ascii')
+#
+# print(answer)
