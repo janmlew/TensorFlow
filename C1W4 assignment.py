ef0f770219d2325849deb30db6706d3443b2a927
janmlew <janmikolajlewinski@gmail.com>
2023-09-10 09:43:58 +0200

    PEP 8 fixups.

--- C1W4 assignment.py
+++ C1W4 assignment.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 from tensorflow.keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 from tensorflow.keras import optimizers, losses
+
 # import base64
 
 base_dir: str = "./C1W4a/"
@@ -19,7 +20,7 @@ plt.imshow(load_img(f"{os.path.join(sad_dir, os.listdir(sad_dir)[0])}"))
 plt.show()
 
 # Load the first example of a happy face
-sample_image  = load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}")
+sample_image = load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}")
 
 # Convert the image into its numpy array representation
 sample_array = img_to_array(sample_image)
@@ -28,19 +29,21 @@ print(f"Each image has shape: {sample_array.shape}")
 
 print(f"The maximum pixel value used is: {np.max(sample_array)}")
 
-class myCallback(tf.keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs={}):
+
+class MyCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
         if logs.get('accuracy') is not None and logs.get('accuracy') > 0.999:
             print("\nReached 99.9% accuracy so cancelling training!")
             self.model.stop_training = True
 
+
 # GRADED FUNCTION: image_generator
 def image_generator():
-    ### START CODE HERE
+    # START CODE HERE
 
     # Instantiate the ImageDataGenerator class.
     # Remember to set the rescale argument.
-    train_datagen = ImageDataGenerator(rescale=1/255)
+    train_datagen = ImageDataGenerator(rescale=1 / 255)
 
     # Specify the method to load images from a directory and pass in the appropriate arguments:
     # - directory: should be a relative path to the directory containing the data
@@ -52,19 +55,21 @@ def image_generator():
                                                         target_size=(150, 150),
                                                         batch_size=10,
                                                         class_mode="binary")
-    ### END CODE HERE
+    # END CODE HERE
 
     return train_generator
+
 
 # Save your generator in a variable
 gen = image_generator()
 
+
 # GRADED FUNCTION: train_happy_sad_model
 def train_happy_sad_model(train_generator):
     # Instantiate the callback
-    callbacks = myCallback()
+    callbacks = MyCallback()
 
-    ### START CODE HERE
+    # START CODE HERE
 
     # Define the model
     model = tf.keras.models.Sequential([
@@ -82,8 +87,8 @@ def train_happy_sad_model(train_generator):
     # Compile the model
     # Select a loss function compatible with the last layer of your network
     model.compile(loss=losses.binary_crossentropy,
-    optimizer = optimizers.RMSprop(learning_rate=0.001),
-    metrics = ['accuracy'])
+                  optimizer=optimizers.RMSprop(learning_rate=0.001),
+                  metrics=['accuracy'])
 
     # Train the model
     # Your model should achieve the desired accuracy in less than 15 epochs.
@@ -93,14 +98,15 @@ def train_happy_sad_model(train_generator):
                         callbacks=[callbacks]
                         )
 
-    ### END CODE HERE
+    # END CODE HERE
     return history
+
 
 hist = train_happy_sad_model(gen)
 
 print(f"Your model reached the desired accuracy after {len(hist.epoch)} epochs")
 
-if not "accuracy" in hist.model.metrics_names:
+if "accuracy" not in hist.model.metrics_names:
     print("Use 'accuracy' as metric when compiling your model.")
 else:
     print("The metric was correctly defined.")
