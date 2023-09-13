dbe19ff1b65775922cb061c149db0fd0175c531a
janmlew <janmikolajlewinski@gmail.com>
2023-09-13 16:34:08 +0200

    Added new PyCharm file and a new .gitignore rule.

---
+++ C2W1.py
@@ -0,0 +1,213 @@
+import zipfile
+import os
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import random
+from tensorflow.keras.utils import img_to_array, load_img
+
+# Unzip the archive
+local_zip = './cats_and_dogs_filtered.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall()
+
+zip_ref.close()
+
+base_dir = 'cats_and_dogs_filtered'
+
+print("Contents of base directory:")
+print(os.listdir(base_dir))
+
+print("\nContents of train directory:")
+print(os.listdir(f'{base_dir}/train'))
+
+print("\nContents of validation directory:")
+print(os.listdir(f'{base_dir}/validation'))
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with training cat/dog pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with validation cat/dog pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+train_cat_fnames = os.listdir(train_cats_dir)
+train_dog_fnames = os.listdir(train_dogs_dir)
+
+print(train_cat_fnames[:10])
+print(train_dog_fnames[:10])
+
+print('total training cat images :', len(os.listdir(train_cats_dir)))
+print('total training dog images :', len(os.listdir(train_dogs_dir)))
+
+print('total validation cat images :', len(os.listdir(validation_cats_dir)))
+print('total validation dog images :', len(os.listdir(validation_dogs_dir)))
+
+# Parameters for our graph; we'll output images in a 4x4 configuration
+nrows = 4
+ncols = 4
+
+pic_index = 0  # Index for iterating over images
+
+# Set up matplotlib fig, and size it to fit 4x4 pics
+fig = plt.gcf()
+fig.set_size_inches(ncols * 4, nrows * 4)
+
+pic_index += 8
+
+next_cat_pix = [os.path.join(train_cats_dir, fname)
+                for fname in train_cat_fnames[pic_index - 8:pic_index]
+                ]
+
+next_dog_pix = [os.path.join(train_dogs_dir, fname)
+                for fname in train_dog_fnames[pic_index - 8:pic_index]
+                ]
+
+for i, img_path in enumerate(next_cat_pix + next_dog_pix):
+    # Set up subplot; subplot indices start at 1
+    sp = plt.subplot(nrows, ncols, i + 1)
+    sp.axis('Off')  # Don't show axes (or gridlines)
+
+    img = mpimg.imread(img_path)
+    plt.imshow(img)
+
+plt.show()
+
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 150x150 with 3 bytes color
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('cats') and 1 for the other ('dogs')
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+model.compile(optimizer=RMSprop(learning_rate=0.001),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# All images will be rescaled by 1./255.
+train_datagen = ImageDataGenerator(rescale=1.0 / 255.)
+test_datagen = ImageDataGenerator(rescale=1.0 / 255.)
+
+# --------------------
+# Flow training images in batches of 20 using train_datagen generator
+# --------------------
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size=20,
+                                                    class_mode='binary',
+                                                    target_size=(150, 150))
+# --------------------
+# Flow validation images in batches of 20 using test_datagen generator
+# --------------------
+validation_generator = test_datagen.flow_from_directory(validation_dir,
+                                                        batch_size=20,
+                                                        class_mode='binary',
+                                                        target_size=(150, 150))
+
+history = model.fit(
+    train_generator,
+    epochs=15,
+    validation_data=validation_generator,
+    verbose=2
+)
+
+# Define a new Model that will take an image as input, and will output
+# intermediate representations for all layers in the previous model
+successive_outputs = [layer.output for layer in model.layers]
+visualization_model = tf.keras.models.Model(inputs=model.input, outputs=successive_outputs)
+
+# Prepare a random input image from the training set.
+cat_img_files = [os.path.join(train_cats_dir, f) for f in train_cat_fnames]
+dog_img_files = [os.path.join(train_dogs_dir, f) for f in train_dog_fnames]
+img_path = random.choice(cat_img_files + dog_img_files)
+img = load_img(img_path, target_size=(150, 150))  # this is a PIL image
+x = img_to_array(img)  # Numpy array with shape (150, 150, 3)
+x = x.reshape((1,) + x.shape)  # Numpy array with shape (1, 150, 150, 3)
+
+# Scale by 1/255
+x /= 255.0
+
+# Run the image through the network, thus obtaining all
+# intermediate representations for this image.
+successive_feature_maps = visualization_model.predict(x)
+
+# These are the names of the layers, so you can have them as part of our plot
+layer_names = [layer.name for layer in model.layers]
+
+# Display the representations
+for layer_name, feature_map in zip(layer_names, successive_feature_maps):
+
+    if len(feature_map.shape) == 4:
+
+        # -------------------------------------------
+        # Just do this for the conv / maxpool layers, not the fully-connected layers
+        # -------------------------------------------
+        n_features = feature_map.shape[-1]  # number of features in the feature map
+        size = feature_map.shape[1]  # feature map shape (1, size, size, n_features)
+
+        # Tile the images in this matrix
+        display_grid = np.zeros((size, size * n_features))
+
+        # -------------------------------------------------
+        # Postprocess the feature to be visually palatable
+        # -------------------------------------------------
+        for i in range(n_features):
+            x = feature_map[0, :, :, i]
+            x -= x.mean()
+            x /= x.std()
+            x *= 64
+            x += 128
+            x = np.clip(x, 0, 255).astype('uint8')
+            display_grid[:, i * size: (i + 1) * size] = x  # Tile each filter into a horizontal grid
+
+        # -----------------
+        # Display the grid
+        # -----------------
+        scale = 20. / n_features
+        plt.figure(figsize=(scale * n_features, scale))
+        plt.title(layer_name)
+        plt.grid(False)
+        plt.imshow(display_grid, aspect='auto', cmap='viridis')
+
+# -----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+# -----------------------------------------------------------
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))  # Get number of epochs
+
+# ------------------------------------------------
+# Plot training and validation accuracy per epoch
+# ------------------------------------------------
+plt.plot(epochs, acc)
+plt.plot(epochs, val_acc)
+plt.title('Training and validation accuracy')
+plt.figure()
+
+# ------------------------------------------------
+# Plot training and validation loss per epoch
+# ------------------------------------------------
+plt.plot(epochs, loss)
+plt.plot(epochs, val_loss)
+plt.title('Training and validation loss')
