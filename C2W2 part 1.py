2f220dbb62306ce51713cc120dbe4b2dced29321
janmlew <janmikolajlewinski@gmail.com>
2023-09-29 11:58:54 +0200

    New PyCharm file + resulting images.

---
+++ C2W2 part 1.py
@@ -0,0 +1,157 @@
+import os
+import zipfile
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+# Extract the archive
+zip_ref = zipfile.ZipFile("./cats_and_dogs_filtered.zip", 'r')
+zip_ref.extractall("tmp/")
+zip_ref.close()
+
+# Assign training and validation set directories
+base_dir = 'tmp/cats_and_dogs_filtered'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+
+# Directory with training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+
+# Directory with validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+
+def create_model():
+    """Creates a CNN with 4 convolutional layers"""
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer=RMSprop(learning_rate=1e-4),
+                  metrics=['accuracy'])
+
+    return model
+
+
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+    train_dir,  # This is the source directory for training images
+    target_size=(150, 150),  # All images will be resized to 150x150
+    batch_size=20,
+    # Since we use binary_crossentropy loss, we need binary labels
+    class_mode='binary')
+
+# Flow validation images in batches of 20 using test_datagen generator
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+# Constant for epochs
+EPOCHS = 20
+
+# Create a new model
+model = create_model()
+
+# Train the model
+history = model.fit(
+    train_generator,
+    steps_per_epoch=100,  # 2000 images = batch_size * steps
+    epochs=EPOCHS,
+    validation_data=validation_generator,
+    validation_steps=50,  # 1000 images = batch_size * steps
+    verbose=2)
+
+
+def plot_loss_acc(history):
+    """Plots the training and validation loss and accuracy from a history object"""
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(acc))
+
+    plt.plot(epochs, acc, 'bo', label='Training accuracy')
+    plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training Loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    plt.show()
+
+
+# Plot training results
+plot_loss_acc(history)
+
+# Create new model
+model_for_aug = create_model()
+
+# This code has changed. Now instead of the ImageGenerator just rescaling
+# the image, we also rotate and do other operations
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+    train_dir,  # This is the source directory for training images
+    target_size=(150, 150),  # All images will be resized to 150x150
+    batch_size=20,
+    # Since we use binary_crossentropy loss, we need binary labels
+    class_mode='binary')
+
+# Flow validation images in batches of 20 using test_datagen generator
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+# Train the new model
+history_with_aug = model_for_aug.fit(
+    train_generator,
+    steps_per_epoch=100,  # 2000 images = batch_size * steps
+    epochs=EPOCHS,
+    validation_data=validation_generator,
+    validation_steps=50,  # 1000 images = batch_size * steps
+    verbose=2)
+
+# Plot the results of training with data augmentation
+plot_loss_acc(history_with_aug)
