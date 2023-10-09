e7f062f6ba7db958bf3fac0256807fed6725d60a
janmlew <janmikolajlewinski@gmail.com>
2023-10-09 11:39:39 +0200

    Solved assignment written in PyCharm.

--- C2W4 assignment.py
+++ C2W4 assignment.py
@@ -14,3 +14,204 @@ with open(TRAINING_FILE) as training_file:
   line = training_file.readline()
   print(f"Each subsequent line (data points) look like this:\n{line}")
 
+
+# GRADED FUNCTION: parse_data_from_input
+def parse_data_from_input(filename):
+  """
+  Parses the images and labels from a CSV file
+
+  Args:
+    filename (string): path to the CSV file
+
+  Returns:
+    images, labels: tuple of numpy arrays containing the images and labels
+  """
+
+  """
+
+  labels = []
+  images = []
+
+  with open(filename) as file:
+    ### START CODE HERE
+
+    # Use csv.reader, passing in the appropriate delimiter
+    # Remember that csv.reader can be iterated and returns one line in each iteration
+    csv_reader = csv.reader(file, delimiter=',')
+
+    for row in csv_reader:
+      labels.append(row[0])
+      images.append(row[1:])
+
+  labels.pop(0)
+  images.pop(0)
+  #or:
+  #del labels[0]
+  #del images[0]
+
+  labels = np.array(labels, dtype=np.float64)
+  images = np.array(images, dtype=np.float64).reshape(len(labels), 28, 28)
+
+  #Alternative to csv.reader:
+  #f = np.loadtxt(TRAINING_FILE, delimiter=',', skiprows=1)
+  """
+  # I think the second version is more elegant:
+
+  images = np.loadtxt(filename, delimiter=',', skiprows=1)
+
+  labels = images[:, 0]
+  images = images[:, 1:]
+
+  labels = np.array(labels, dtype=np.float64)
+  images = np.array(images, dtype=np.float64).reshape(len(labels), 28, 28)
+
+  ### END CODE HERE
+
+  return images, labels
+
+# Test your function
+training_images, training_labels = parse_data_from_input(TRAINING_FILE)
+validation_images, validation_labels = parse_data_from_input(VALIDATION_FILE)
+
+print(f"Training images has shape: {training_images.shape} and dtype: {training_images.dtype}")
+print(f"Training labels has shape: {training_labels.shape} and dtype: {training_labels.dtype}")
+print(f"Validation images has shape: {validation_images.shape} and dtype: {validation_images.dtype}")
+print(f"Validation labels has shape: {validation_labels.shape} and dtype: {validation_labels.dtype}")
+
+# Plot a sample of 10 images from the training set
+def plot_categories(training_images, training_labels):
+  fig, axes = plt.subplots(1, 10, figsize=(16, 15))
+  axes = axes.flatten()
+  letters = list(string.ascii_lowercase)
+
+  for k in range(10):
+    img = training_images[k]
+    img = np.expand_dims(img, axis=-1)
+    img = array_to_img(img)
+    ax = axes[k]
+    ax.imshow(img, cmap="Greys_r")
+    ax.set_title(f"{letters[int(training_labels[k])]}")
+    ax.set_axis_off()
+
+  plt.tight_layout()
+  plt.show()
+
+plot_categories(training_images, training_labels)
+
+# GRADED FUNCTION: train_val_generators
+def train_val_generators(training_images, training_labels, validation_images, validation_labels):
+  """
+  Creates the training and validation data generators
+
+  Args:
+    training_images (array): parsed images from the train CSV file
+    training_labels (array): parsed labels from the train CSV file
+    validation_images (array): parsed images from the test CSV file
+    validation_labels (array): parsed labels from the test CSV file
+
+  Returns:
+    train_generator, validation_generator - tuple containing the generators
+  """
+  ### START CODE HERE
+
+  # In this section you will have to add another dimension to the data
+  # So, for example, if your array is (10000, 28, 28)
+  # You will need to make it (10000, 28, 28, 1)
+  # Hint: np.expand_dims
+  training_images = np.expand_dims(training_images, axis=3)
+  validation_images = np.expand_dims(validation_images, axis=3)
+
+  # Instantiate the ImageDataGenerator class
+  # Don't forget to normalize pixel values
+  # and set arguments to augment the images (if desired)
+  train_datagen = ImageDataGenerator(rescale = 1./255.,
+                                     rotation_range=40,
+                                     width_shift_range=0.2,
+                                     height_shift_range=0.2,
+                                     shear_range=0.2,
+                                     zoom_range=0.2,
+                                     horizontal_flip=True,
+                                     fill_mode='nearest')
+
+  # Pass in the appropriate arguments to the flow method
+  train_generator = train_datagen.flow(x=training_images,
+                                       y=training_labels,
+                                       batch_size=32)
+                                       #subset='training')
+
+  # Instantiate the ImageDataGenerator class (don't forget to set the rescale argument)
+  # Remember that validation data should not be augmented
+  validation_datagen = ImageDataGenerator(rescale = 1./255.)
+
+  # Pass in the appropriate arguments to the flow method
+  validation_generator = validation_datagen.flow(x=validation_images,
+                                                 y=validation_labels,
+                                                 batch_size=32)
+                                                 #subset='validation')
+
+  ### END CODE HERE
+
+  return train_generator, validation_generator
+
+# Test your generators
+train_generator, validation_generator = train_val_generators(training_images, training_labels, validation_images, validation_labels)
+
+print(f"Images of training generator have shape: {train_generator.x.shape}")
+print(f"Labels of training generator have shape: {train_generator.y.shape}")
+print(f"Images of validation generator have shape: {validation_generator.x.shape}")
+print(f"Labels of validation generator have shape: {validation_generator.y.shape}")
+
+def create_model():
+
+  ### START CODE HERE
+
+  # Define the model
+  # Use no more than 2 Conv2D and 2 MaxPooling2D
+  model = tf.keras.models.Sequential([
+      tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+      tf.keras.layers.MaxPooling2D(2, 2),
+      tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2, 2),
+      tf.keras.layers.Flatten(),
+      #tf.keras.layers.Dropout(0.5),
+      tf.keras.layers.Dense(512, activation='relu'),
+      tf.keras.layers.Dense(25, activation='softmax')
+  ])
+
+  model.compile(loss = 'sparse_categorical_crossentropy',
+                optimizer = 'rmsprop',
+                metrics=['accuracy'])
+
+  ### END CODE HERE
+
+  return model
+
+# Save your model
+model = create_model()
+model.summary()
+
+# Train your model
+history = model.fit(train_generator,
+                    epochs=15,
+                    validation_data=validation_generator)
+
+# Plot the chart for accuracy and loss on both training and validation
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, loss, 'r', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
