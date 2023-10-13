cd34515dca1f50aeae8f43c0003c0677c472eb40
janmlew <janmikolajlewinski@gmail.com>
2023-10-13 17:16:24 +0200

    Added a version of the solution for PyCharm + 2 output files.

--- C3W2 assignment.py
+++ C3W2 assignment.py
@@ -86,3 +86,225 @@ print(f"There are {len(labels)} labels in the dataset.\n")
 print(f"The first 5 labels are {labels[:5]}")
 
 
+# GRADED FUNCTIONS: train_val_split
+def train_val_split(sentences, labels, training_split):
+    """
+    Splits the dataset into training and validation sets
+
+    Args:
+        sentences (list of string): lower-cased sentences without stopwords
+        labels (list of string): list of labels
+        training_split (float): proportion of the dataset to convert to include in the train set
+
+    Returns:
+        train_sentences, validation_sentences, train_labels, validation_labels - lists containing the data splits
+    """
+
+    # START CODE HERE
+
+    # Compute the number of sentences that will be used for training (should be an integer)
+    train_size = int(len(sentences) * training_split)
+
+    # Split the sentences and labels into train/validation splits
+    train_sentences = sentences[0:train_size]
+    train_labels = labels[0:train_size]
+
+    validation_sentences = sentences[train_size:]
+    validation_labels = labels[train_size:]
+
+    # END CODE HERE
+
+    return train_sentences, validation_sentences, train_labels, validation_labels
+
+
+# Test your function
+train_sentences, val_sentences, train_labels, val_labels = train_val_split(sentences, labels, TRAINING_SPLIT)
+
+print(f"There are {len(train_sentences)} sentences for training.\n")
+print(f"There are {len(train_labels)} labels for training.\n")
+print(f"There are {len(val_sentences)} sentences for validation.\n")
+print(f"There are {len(val_labels)} labels for validation.")
+
+
+# GRADED FUNCTION: fit_tokenizer
+def fit_tokenizer(train_sentences, num_words, oov_token):
+    """
+    Instantiates the Tokenizer class on the training sentences
+
+    Args:
+        train_sentences (list of string): lower-cased sentences without stopwords to be used for training
+        num_words (int) - number of words to keep when tokenizing
+        oov_token (string) - symbol for the out-of-vocabulary token
+
+    Returns:
+        tokenizer (object): an instance of the Tokenizer class containing the word-index dictionary
+    """
+
+    # START CODE HERE
+
+    # Instantiate the Tokenizer class, passing in the correct values for num_words and oov_token
+    tokenizer = Tokenizer(num_words=num_words, oov_token=oov_token)
+
+    # Fit the tokenizer to the training sentences
+    tokenizer.fit_on_texts(train_sentences)
+
+    # END CODE HERE
+
+    return tokenizer
+
+
+# Test your function
+tokenizer = fit_tokenizer(train_sentences, NUM_WORDS, OOV_TOKEN)
+word_index = tokenizer.word_index
+
+print(f"Vocabulary contains {len(word_index)} words\n")
+print("<OOV> token included in vocabulary" if "<OOV>" in word_index else "<OOV> token NOT included in vocabulary")
+
+
+# GRADED FUNCTION: seq_and_pad
+def seq_and_pad(sentences, tokenizer, padding, maxlen):
+    """
+    Generates an array of token sequences and pads them to the same length
+
+    Args:
+        sentences (list of string): list of sentences to tokenize and pad
+        tokenizer (object): Tokenizer instance containing the word-index dictionary
+        padding (string): type of padding to use
+        maxlen (int): maximum length of the token sequence
+
+    Returns:
+        padded_sequences (array of int): tokenized sentences padded to the same length
+    """
+    # START CODE HERE
+
+    # Convert sentences to sequences
+    sequences = tokenizer.texts_to_sequences(sentences)
+
+    # Pad the sequences using the correct padding and maxlen
+    padded_sequences = pad_sequences(sequences, maxlen=maxlen, padding=padding)
+
+    # END CODE HERE
+
+    return padded_sequences
+
+
+# Test your function
+train_padded_seq = seq_and_pad(train_sentences, tokenizer, PADDING, MAXLEN)
+val_padded_seq = seq_and_pad(val_sentences, tokenizer, PADDING, MAXLEN)
+
+print(f"Padded training sequences have shape: {train_padded_seq.shape}\n")
+print(f"Padded validation sequences have shape: {val_padded_seq.shape}")
+
+
+# GRADED FUNCTION: tokenize_labels
+def tokenize_labels(all_labels, split_labels):
+    """
+    Tokenizes the labels
+
+    Args:
+        all_labels (list of string): labels to generate the word-index from
+        split_labels (list of string): labels to tokenize
+
+    Returns:
+        label_seq_np (array of int): tokenized labels
+    """
+    # START CODE HERE
+
+    # Instantiate the Tokenizer (no additional arguments needed)
+    label_tokenizer = Tokenizer(num_words=len(all_labels))
+
+    # Fit the tokenizer on all the labels
+    label_tokenizer.fit_on_texts(all_labels)
+
+    # Convert labels to sequences
+    label_seq = label_tokenizer.texts_to_sequences(split_labels)
+
+    # Convert sequences to a numpy array. Don't forget to substact 1 from every entry in the array!
+    label_seq_np = np.array(label_seq) - 1
+
+    # END CODE HERE
+
+    return label_seq_np
+
+
+# Test your function
+train_label_seq = tokenize_labels(labels, train_labels)
+val_label_seq = tokenize_labels(labels, val_labels)
+
+print(f"First 5 labels of the training set should look like this:\n{train_label_seq[:5]}\n")
+print(f"First 5 labels of the validation set should look like this:\n{val_label_seq[:5]}\n")
+print(f"Tokenized labels of the training set have shape: {train_label_seq.shape}\n")
+print(f"Tokenized labels of the validation set have shape: {val_label_seq.shape}\n")
+
+
+# GRADED FUNCTION: create_model
+def create_model(num_words, embedding_dim, maxlen):
+    """
+    Creates a text classifier model
+
+    Args:
+        num_words (int): size of the vocabulary for the Embedding layer input
+        embedding_dim (int): dimensionality of the Embedding layer output
+        maxlen (int): length of the input sequences
+
+    Returns:
+        model (tf.keras Model): the text classifier model
+    """
+
+    tf.random.set_seed(123)
+
+    # START CODE HERE
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(num_words, embedding_dim, input_length=maxlen),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(25, activation='relu'),
+        tf.keras.layers.Dense(5, activation='softmax')
+    ])
+
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    # END CODE HERE
+
+    return model
+
+
+model = create_model(NUM_WORDS, EMBEDDING_DIM, MAXLEN)
+
+history = model.fit(train_padded_seq, train_label_seq, epochs=30, validation_data=(val_padded_seq, val_label_seq))
+
+
+def plot_graphs(history, metric):
+    plt.plot(history.history[metric])
+    plt.plot(history.history[f'val_{metric}'])
+    plt.xlabel("Epochs")
+    plt.ylabel(metric)
+    plt.legend([metric, f'val_{metric}'])
+    plt.show()
+
+
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")
+
+# Reverse word index
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+# Save the embedding layer
+e = model.layers[0]
+
+# Save the weights of the embedding layer
+weights = e.get_weights()[0]
+print(f"Weights of embedding layer have shape: {weights.shape}")
+
+# Generate files for embedding visualization
+out_v = io.open('vecs_pyc_c3w2.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta_pyc_c3w2.tsv', 'w', encoding='utf-8')
+for word_num in range(1, NUM_WORDS):
+    word = reverse_word_index[word_num]
+    embeddings = weights[word_num]
+    out_m.write(word + "\n")
+    out_v.write('\t'.join([str(x) for x in embeddings]) + "\n")
+out_v.close()
+out_m.close()
