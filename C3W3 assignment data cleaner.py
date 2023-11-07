695919cb2e1eafc8ce9d7d6f08a21fab0b36295d
janmlew <janmikolajlewinski@gmail.com>
2023-11-07 12:20:25 +0100

    Rewritten my data cleaner. Now it correctly rewrites data files in order to avoid encoding errors. Updated a PyCharm project file.

--- C3W3 assignment data cleaner.py
+++ C3W3 assignment data cleaner.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import os
 
 training = pd.read_csv('training.csv', encoding_errors="ignore")
 training.to_csv('training_cleaned.csv', index=False, encoding='utf-8')
 
-with open('glove_unclean.txt')
+if os.path.exists("glove.6B.100d.txt"):
+    os.remove("glove.6B.100d.txt")
+
+with open('glove_unclean.txt', 'rb') as f:
+    with open('glove.6B.100d.txt', 'w', errors='ignore') as textfile:
+        textfile.write(f.read().decode('utf-8', errors='ignore'))
