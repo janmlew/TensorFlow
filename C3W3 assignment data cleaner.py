7f24a6b56f0fe2bd1aa453384dc87b8a33ce8556
janmlew <janmikolajlewinski@gmail.com>
2023-11-08 12:42:23 +0100

    Cleanup + further code.

--- C3W3 assignment data cleaner.py
+++ C3W3 assignment data cleaner.py
@@ -2,8 +2,8 @@ import pandas as pd
 import os
 import string
 
-# training = pd.read_csv('training.csv', encoding_errors="ignore")
-# training.to_csv('training_cleaned.csv', index=False, encoding='utf-8')
+training = pd.read_csv('training.csv', encoding_errors="ignore")
+training.to_csv('training_cleaned.csv', index=False, encoding='utf-8')
 
 if os.path.exists("glove.6B.100d.txt"):
     os.remove("glove.6B.100d.txt")
@@ -11,11 +11,7 @@ if os.path.exists("glove.6B.100d.txt"):
 with open('glove_unclean.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
-# Usuń znaki spoza zakresu liter łacińskich
 text = ''.join(filter(lambda x: x in string.printable, text))
-
-# Zamień znaki diakrytyczne na ich odpowiedniki bez znaków diakrytycznych
-# text = text.translate(str.maketrans('ąćęłńóśźżĄĆĘŁŃÓŚŹŻ', 'acelnoszzACELNOSZZ'))
 
 with open('glove.6B.100d.txt', 'w', encoding='utf-8') as f:
     f.write(text)
--- C3W3 assignment.py
+++ C3W3 assignment.py
@@ -1,6 +1,5 @@
 import csv
 import random
-import pickle
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -24,18 +23,17 @@ with open(SENTIMENT_CSV, 'r') as csvfile:
 
 
 def parse_data_from_file(filename):
-
     sentences = []
     labels = []
 
     with open(filename, 'r', errors='ignore') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
-          if row[0]==0:
-            labels.append(int(0))
-          else:
-            labels.append(int(1))
-          sentences.append(row[5])
+            if row[0] == 0:
+                labels.append(int(0))
+            else:
+                labels.append(int(1))
+            sentences.append(row[5])
 
     return sentences, labels
 
@@ -60,7 +58,6 @@ print(f"There are {len(sentences)} sentences and {len(labels)} labels after rand
 
 
 def train_val_split(sentences, labels, training_split):
-
     train_size = int(len(sentences) * training_split)
 
     train_sentences = sentences[0:train_size]
@@ -81,7 +78,6 @@ print(f"There are {len(val_labels)} labels for validation.")
 
 
 def fit_tokenizer(train_sentences, oov_token):
-
     tokenizer = Tokenizer(oov_token=oov_token)
     tokenizer.fit_on_texts(train_sentences)
 
@@ -96,6 +92,34 @@ VOCAB_SIZE = len(word_index)
 print(f"Vocabulary contains {VOCAB_SIZE} words\n")
 print("<OOV> token included in vocabulary" if "<OOV>" in word_index else "<OOV> token NOT included in vocabulary")
 print(f"\nindex of word 'i' should be {word_index['i']}")
+
+
+def seq_pad_and_trunc(sentences, tokenizer, padding, truncating, maxlen):
+    """
+    Generates an array of token sequences and pads them to the same length
+
+    Args:
+        sentences (list of string): list of sentences to tokenize and pad
+        tokenizer (object): Tokenizer instance containing the word-index dictionary
+        padding (string): type of padding to use
+        truncating (string): type of truncating to use
+        maxlen (int): maximum length of the token sequence
+
+    Returns:
+        pad_trunc_sequences (array of int): tokenized sentences padded to the same length
+    """
+
+    sequences = tokenizer.texts_to_sequences(sentences)
+    pad_trunc_sequences = pad_sequences(sequences, maxlen=maxlen, padding=padding, truncating=truncating)
+
+    return pad_trunc_sequences
+
+
+train_pad_trunc_seq = seq_pad_and_trunc(train_sentences, tokenizer, PADDING, TRUNCATING, MAXLEN)
+val_pad_trunc_seq = seq_pad_and_trunc(val_sentences, tokenizer, PADDING, TRUNCATING, MAXLEN)
+
+print(f"Padded and truncated training sequences have shape: {train_pad_trunc_seq.shape}\n")
+print(f"Padded and truncated validation sequences have shape: {val_pad_trunc_seq.shape}")
 
 train_labels = np.array(train_labels)
 val_labels = np.array(val_labels)
@@ -118,3 +142,131 @@ test_vector = GLOVE_EMBEDDINGS[test_word]
 print(f"Vector representation of word {test_word} looks like this:\n\n{test_vector}")
 print(f"Each word vector has shape: {test_vector.shape}")
 
+EMBEDDINGS_MATRIX = np.zeros((VOCAB_SIZE + 1, EMBEDDING_DIM))
+
+for word, i in word_index.items():
+    embedding_vector = GLOVE_EMBEDDINGS.get(word)
+    if embedding_vector is not None:
+        EMBEDDINGS_MATRIX[i] = embedding_vector
+
+
+# grader-required-cell
+
+# GRADED FUNCTION: create_model
+def create_model(vocab_size, embedding_dim, maxlen, embeddings_matrix):
+    """
+    Creates a binary sentiment classifier model
+
+    Args:
+        vocab_size (int): size of the vocabulary for the Embedding layer input
+        embedding_dim (int): dimensionality of the Embedding layer output
+        maxlen (int): length of the input sequences
+        embeddings_matrix (array): predefined weights of the embeddings
+
+    Returns:
+        model (tf.keras Model): the sentiment classifier model
+    """
+    ### START CODE HERE
+
+    '''
+    model = tf.keras.Sequential([
+        # This is how you need to set the Embedding layer when using pre-trained embeddings
+        tf.keras.layers.Embedding(vocab_size+1, embedding_dim, input_length=maxlen, weights=[embeddings_matrix], trainable=False),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model = tf.keras.Sequential([
+        # This is how you need to set the Embedding layer when using pre-trained embeddings
+        tf.keras.layers.Embedding(vocab_size+1, embedding_dim, input_length=maxlen, weights=[embeddings_matrix], trainable=False),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model = tf.keras.Sequential([
+        # This is how you need to set the Embedding layer when using pre-trained embeddings
+        tf.keras.layers.Embedding(vocab_size+1, embedding_dim, input_length=maxlen, weights=[embeddings_matrix], trainable=False),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Conv1D(filters=128, kernel_size=5, activation='relu'),
+        tf.keras.layers.GlobalMaxPooling1D(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+    '''
+
+    model = tf.keras.Sequential([
+        # This is how you need to set the Embedding layer when using pre-trained embeddings
+        tf.keras.layers.Embedding(vocab_size + 1, embedding_dim, input_length=maxlen, weights=[embeddings_matrix],
+                                  trainable=False),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Conv1D(128, 5, activation='relu'),
+        tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.Dense(6, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    ### END CODE HERE
+
+    return model
+
+
+# grader-required-cell
+
+# Create your untrained model
+model = create_model(VOCAB_SIZE, EMBEDDING_DIM, MAXLEN, EMBEDDINGS_MATRIX)
+
+# Train the model and save the training history
+history = model.fit(train_pad_trunc_seq, train_labels, epochs=20, validation_data=(val_pad_trunc_seq, val_labels))
+
+# grader-required-cell
+
+# -----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+# -----------------------------------------------------------
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = [*range(20)]
+
+# ------------------------------------------------
+# Plot training and validation loss per epoch
+# ------------------------------------------------
+plt.plot(epochs, loss, 'r')
+plt.plot(epochs, val_loss, 'b')
+plt.title('Training and validation loss')
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend(["Loss", "Validation Loss"])
+plt.show()
+
+# grader-required-cell
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+# ------------------------------------------------
+# Plot training and validation accuracy per epoch
+# ------------------------------------------------
+plt.plot(epochs, acc, 'r')
+plt.plot(epochs, val_acc, 'b')
+plt.title('Training and validation accuracy')
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend(["Accuracy", "Validation Accuracy"])
+plt.show()
+
+# grader-required-cell
+
+# Test the slope of your val_loss curve
+slope, *_ = linregress(epochs, val_loss)
+print(f"The slope of your validation loss curve is {slope:.5f}")
