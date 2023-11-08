516eda59605dd79abe61b802c6235827c1a8f4bd
janmlew <janmikolajlewinski@gmail.com>
2023-11-08 18:56:40 +0100

    Added new files to PyCharm (including code processed for PyCharm).

---
+++ C3W4 part 2.py
@@ -0,0 +1,202 @@
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+import matplotlib.pyplot as plt
+
+# Load the dataset
+data = open('irish-lyrics-eof.txt').read()
+
+# Lowercase and split the text
+corpus = data.lower().split("\n")
+
+# Preview the result
+print(corpus)
+
+# Initialize the Tokenizer class
+tokenizer = Tokenizer()
+
+# Generate the word index dictionary
+tokenizer.fit_on_texts(corpus)
+
+# Define the total words. You add 1 for the index `0` which is just the padding token.
+total_words = len(tokenizer.word_index) + 1
+
+print(f'word index dictionary: {tokenizer.word_index}')
+print(f'total words: {total_words}')
+
+# Initialize the sequences list
+input_sequences = []
+
+# Loop over every line
+for line in corpus:
+
+    # Tokenize the current line
+    token_list = tokenizer.texts_to_sequences([line])[0]
+
+    # Loop over the line several times to generate the subphrases
+    for i in range(1, len(token_list)):
+        # Generate the subphrase
+        n_gram_sequence = token_list[:i + 1]
+
+        # Append the subphrase to the sequences list
+        input_sequences.append(n_gram_sequence)
+
+# Get the length of the longest line
+max_sequence_len = max([len(x) for x in input_sequences])
+
+# Pad all sequences
+input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre')
+
+# Create inputs and label by splitting the last token in the subphrases
+xs, labels = input_sequences[:, :-1], input_sequences[:, -1]
+
+# Convert the label into one-hot arrays
+ys = tf.keras.utils.to_categorical(labels, num_classes=total_words)
+
+# Get sample sentence
+sentence = corpus[0].split()
+print(f'sample sentence: {sentence}')
+
+# Initialize token list
+token_list = []
+
+# Look up the indices of each word and append to the list
+for word in sentence:
+    token_list.append(tokenizer.word_index[word])
+
+# Print the token list
+print(token_list)
+
+# Pick element
+elem_number = 5
+
+# Print token list and phrase
+print(f'token list: {xs[elem_number]}')
+print(f'decoded to text: {tokenizer.sequences_to_texts([xs[elem_number]])}')
+
+# Print label
+print(f'one-hot label: {ys[elem_number]}')
+print(f'index of label: {np.argmax(ys[elem_number])}')
+
+# Pick element
+elem_number = 4
+
+# Print token list and phrase
+print(f'token list: {xs[elem_number]}')
+print(f'decoded to text: {tokenizer.sequences_to_texts([xs[elem_number]])}')
+
+# Print label
+print(f'one-hot label: {ys[elem_number]}')
+print(f'index of label: {np.argmax(ys[elem_number])}')
+
+# Hyperparameters
+embedding_dim = 100
+lstm_units = 150
+learning_rate = 0.01
+
+# Build the model
+model = Sequential([
+    Embedding(total_words, embedding_dim, input_length=max_sequence_len - 1),
+    Bidirectional(LSTM(lstm_units)),
+    Dense(total_words, activation='softmax')
+])
+
+# Use categorical crossentropy because this is a multi-class problem
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+    metrics=['accuracy']
+)
+
+# Print the model summary
+model.summary()
+
+epochs = 100
+
+# Train the model
+history = model.fit(xs, ys, epochs=epochs)
+
+
+# Plot utility
+def plot_graphs(history, string):
+    plt.plot(history.history[string])
+    plt.xlabel("Epochs")
+    plt.ylabel(string)
+    plt.show()
+
+
+# Visualize the accuracy
+plot_graphs(history, 'accuracy')
+
+# Define seed text
+seed_text = "help me obi-wan kinobi youre my only hope"
+
+# Define total words to predict
+next_words = 100
+
+# Loop until desired length is reached
+for _ in range(next_words):
+
+    # Convert the seed text to a token sequence
+    token_list = tokenizer.texts_to_sequences([seed_text])[0]
+
+    # Pad the sequence
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
+
+    # Feed to the model and get the probabilities for each index
+    probabilities = model.predict(token_list, verbose=0)
+
+    # Get the index with the highest probability
+    predicted = np.argmax(probabilities, axis=-1)[0]
+
+    # Ignore if index is 0 because that is just the padding.
+    if predicted != 0:
+        # Look up the word associated with the index.
+        output_word = tokenizer.index_word[predicted]
+
+        # Combine with the seed text
+        seed_text += " " + output_word
+
+# Print the result
+print(seed_text)
+
+# Define seed text
+seed_text = "help me obi-wan kinobi youre my only hope"
+
+# Define total words to predict
+next_words = 100
+
+# Loop until desired length is reached
+for _ in range(next_words):
+
+    # Convert the seed text to a token sequence
+    token_list = tokenizer.texts_to_sequences([seed_text])[0]
+
+    # Pad the sequence
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
+
+    # Feed to the model and get the probabilities for each index
+    probabilities = model.predict(token_list, verbose=0)
+
+    # Pick a random number from [1,2,3]
+    choice = np.random.choice([1, 2, 3])
+
+    # Sort the probabilities in ascending order
+    # and get the random choice from the end of the array
+    predicted = np.argsort(probabilities)[0][-choice]
+
+    # Ignore if index is 0 because that is just the padding.
+    if predicted != 0:
+        # Look up the word associated with the index.
+        output_word = tokenizer.index_word[predicted]
+
+        # Combine with the seed text
+        seed_text += " " + output_word
+
+# Print the result
+print(seed_text)
