2bf5e0466c47e1e575567a1ace54fbcf2f692b6b
janmlew <janmikolajlewinski@gmail.com>
2023-11-16 13:03:24 +0100

    Minor fixup.

--- C3W4 part 2.py
+++ C3W4 part 2.py
@@ -5,7 +5,6 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
 import matplotlib.pyplot as plt
 
 # Load the dataset
