d928139ca74b26dc31b7a6658ec5a6bf391c58d0
janmlew <janmikolajlewinski@gmail.com>
2023-11-25 19:56:33 +0100

    Added an initial part of code.

--- C3W4 assignment.py
+++ C3W4 assignment.py
@@ -8,5 +8,26 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional
 
 url = 'https://drive.google.com/uc?id=108jAePKK4R3BVYBbYJZ32JWUwxeMg20K'
-out_path = 'sonnets.txt'
-gdown.download(url, out_path, quiet=False)
+SONNETS_FILE = 'sonnets.txt'
+gdown.download(url, SONNETS_FILE, quiet=False)
+
+with open(SONNETS_FILE) as f:
+    data = f.read()
+
+corpus = data.lower().split("\n")
+
+print(f"There are {len(corpus)} lines of sonnets\n")
+print(f"The first 5 lines look like this:\n")
+for i in range(5):
+    print(corpus[i])
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(corpus)
+total_words = len(tokenizer.word_index) + 1
+
+print(corpus[0])
+
+print(tokenizer.texts_to_sequences(corpus[0]))
+print(tokenizer.texts_to_sequences([corpus[0]]))
+print(tokenizer.texts_to_sequences([corpus[0]])[0])
+
