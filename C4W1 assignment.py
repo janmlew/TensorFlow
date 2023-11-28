70fb9effeeef944ec159276a7d97e30131ea8fa6
janmlew <janmikolajlewinski@gmail.com>
2023-11-28 17:00:45 +0100

    Added assignment solved for PyCharm + runtime output files.

--- C4W1 assignment.py
+++ C4W1 assignment.py
@@ -1,33 +1,30 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
+# from tensorflow import keras
 import matplotlib.pyplot as plt
 
+
 def trend(time, slope=0):
-    """A trend over time"""
     return slope * time
 
+
 def seasonal_pattern(season_time):
-    """Just an arbitrary pattern"""
     return np.where(season_time < 0.1,
                     np.cos(season_time * 7 * np.pi),
                     1 / np.exp(5 * season_time))
 
 
 def seasonality(time, period, amplitude=1, phase=0):
-    """Repeats the same pattern at each period"""
     season_time = ((time + phase) % period) / period
     return amplitude * seasonal_pattern(season_time)
 
 
 def noise(time, noise_level=1, seed=None):
-    """Adds noise to the series"""
     rnd = np.random.RandomState(seed)
     return rnd.randn(len(time)) * noise_level
 
 
 def plot_series(time, series, format="-", title="", label=None, start=0, end=None):
-    """Plot the series"""
     plt.plot(time[start:end], series[start:end], format, label=label)
     plt.xlabel("Time")
     plt.ylabel("Value")
@@ -36,43 +33,35 @@ def plot_series(time, series, format="-", title="", label=None, start=0, end=Non
         plt.legend()
     plt.grid(True)
 
+
 # The time dimension or the x-coordinate of the time series
 TIME = np.arange(4 * 365 + 1, dtype="float32")
 
-# Initial series is just a straight line with a y-intercept
 y_intercept = 10
 slope = 0.01
 SERIES = trend(TIME, slope) + y_intercept
 
-# Adding seasonality
 amplitude = 40
 SERIES += seasonality(TIME, period=365, amplitude=amplitude)
 
-# Adding some noise
 noise_level = 2
 SERIES += noise(TIME, noise_level, seed=42)
 
-# Plot the series
 plt.figure(figsize=(10, 6))
 plot_series(TIME, SERIES)
 plt.show()
 
-# Define time step to split the series
 SPLIT_TIME = 1100
 
-# GRADED FUNCTION: train_val_split
+
 def train_val_split(time, series, time_step=SPLIT_TIME):
-
-    ### START CODE HERE
-    time_train = None
-    series_train = None
-    time_valid = None
-    series_valid = None
-    ### END CODE HERE
-
+    time_train = time[:time_step]
+    series_train = series[:time_step]
+    time_valid = time[time_step:]
+    series_valid = series[time_step:]
     return time_train, series_train, time_valid, series_valid
 
-# Test your function
+
 time_train, series_train, time_valid, series_valid = train_val_split(TIME, SERIES)
 
 plt.figure(figsize=(10, 6))
@@ -82,3 +71,128 @@ plt.show()
 plt.figure(figsize=(10, 6))
 plot_series(time_valid, series_valid, title="Validation")
 plt.show()
+
+
+def compute_metrics(true_series, forecast):
+    mse = tf.keras.metrics.mean_squared_error(true_series, forecast).numpy()
+    mae = tf.keras.metrics.mean_absolute_error(true_series, forecast).numpy()
+    return mse, mae
+
+
+# Define some dummy series for testing
+zeros = np.zeros(5)
+ones = np.ones(5)
+
+mse, mae = compute_metrics(zeros, ones)
+print(f"mse: {mse}, mae: {mae} for series of zeros and prediction of ones\n")
+
+mse, mae = compute_metrics(ones, ones)
+print(f"mse: {mse}, mae: {mae} for series of ones and prediction of ones\n")
+
+print(f"metrics are numpy numeric types: {np.issubdtype(type(mse), np.number)}")
+
+naive_forecast = SERIES[SPLIT_TIME - 1:-1]
+
+print(f"validation series has shape: {series_valid.shape}\n")
+print(f"naive forecast has shape: {naive_forecast.shape}\n")
+print(f"comparable with validation series: {series_valid.shape == naive_forecast.shape}")
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, series_valid, label="validation set")
+plot_series(time_valid, naive_forecast, label="naive forecast")
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, series_valid, start=330, end=361, label="validation set")
+plot_series(time_valid, naive_forecast, start=330, end=361, label="naive forecast")
+
+mse, mae = compute_metrics(series_valid, naive_forecast)
+
+print(f"mse: {mse:.2f}, mae: {mae:.2f} for naive forecast")
+
+
+def moving_average_forecast(series, window_size):
+    forecast = []
+    for time in range(len(series) - window_size):
+        forecast.append(series[time:time + window_size].mean())
+    np_forecast = np.array(forecast)
+    return np_forecast
+
+
+print(f"Whole SERIES has {len(SERIES)} elements so the moving average forecast should have {len(SERIES) - 30} elements")
+
+moving_avg = moving_average_forecast(SERIES, window_size=30)
+print(f"moving average forecast with whole SERIES has shape: {moving_avg.shape}\n")
+
+# Slice it so it matches the validation period
+moving_avg = moving_avg[1100 - 30:]
+print(f"moving average forecast after slicing has shape: {moving_avg.shape}\n")
+print(f"comparable with validation series: {series_valid.shape == moving_avg.shape}")
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, series_valid)
+plot_series(time_valid, moving_avg)
+
+mse, mae = compute_metrics(series_valid, moving_avg)
+
+print(f"mse: {mse:.2f}, mae: {mae:.2f} for moving average forecast")
+
+diff_series = SERIES[365:] - SERIES[:-365]
+diff_time = TIME[365:]
+
+print(f"Whole SERIES has {len(SERIES)} elements so the differencing should have {len(SERIES) - 365} elements\n")
+print(f"diff series has shape: {diff_series.shape}\n")
+print(f"x-coordinate of diff series has shape: {diff_time.shape}\n")
+
+plt.figure(figsize=(10, 6))
+plot_series(diff_time, diff_series)
+plt.show()
+
+diff_moving_avg = moving_average_forecast(diff_series, 50)
+
+print(f"moving average forecast with diff series has shape: {diff_moving_avg.shape}\n")
+
+diff_moving_avg = diff_moving_avg[SPLIT_TIME - 365 - 50:]
+
+print(f"moving average forecast with diff series after slicing has shape: {diff_moving_avg.shape}\n")
+print(f"comparable with validation series: {series_valid.shape == diff_moving_avg.shape}")
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, diff_series[1100 - 365:])
+plot_series(time_valid, diff_moving_avg)
+plt.show()
+
+past_series = SERIES[SPLIT_TIME - 365:-365]
+
+print(f"past series has shape: {past_series.shape}\n")
+
+diff_moving_avg_plus_past = past_series + diff_moving_avg
+
+print(f"moving average forecast with diff series plus past has shape: {diff_moving_avg_plus_past.shape}\n")
+print(f"comparable with validation series: {series_valid.shape == diff_moving_avg_plus_past.shape}")
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, series_valid)
+plot_series(time_valid, diff_moving_avg_plus_past)
+plt.show()
+
+mse, mae = compute_metrics(series_valid, diff_moving_avg_plus_past)
+
+print(f"mse: {mse:.2f}, mae: {mae:.2f} for moving average plus past forecast")
+
+smooth_past_series = moving_average_forecast(SERIES[SPLIT_TIME - 370:-360], 10)
+
+print(f"smooth past series has shape: {smooth_past_series.shape}\n")
+
+diff_moving_avg_plus_smooth_past = smooth_past_series + diff_moving_avg
+
+print(f"moving average forecast with diff series plus past has shape: {diff_moving_avg_plus_smooth_past.shape}\n")
+print(f"comparable with validation series: {series_valid.shape == diff_moving_avg_plus_smooth_past.shape}")
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, series_valid)
+plot_series(time_valid, diff_moving_avg_plus_smooth_past)
+plt.show()
+
+mse, mae = compute_metrics(series_valid, diff_moving_avg_plus_smooth_past)
+
+print(f"mse: {mse:.2f}, mae: {mae:.2f} for moving average plus smooth past forecast")
