fc53847d50cac52f8aad7afcf16433dab4ae5f49
janmlew <janmikolajlewinski@gmail.com>
2023-11-30 14:18:02 +0100

    Added a few PEP 8 fixes.

--- C4W2 assignment.py
+++ C4W2 assignment.py
@@ -17,6 +17,7 @@ PADDING = 'post'
 OOV_TOKEN = "<OOV>"
 TRAINING_SPLIT = .8
 
+
 def remove_stopwords(sentence):
     """
     Removes a list of stopwords
@@ -28,7 +29,19 @@ def remove_stopwords(sentence):
         sentence (string): lowercase sentence without the stopwords
     """
     # List of stopwords
-    stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ]
+    stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at",
+                 "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did",
+                 "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have",
+                 "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself",
+                 "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's",
+                 "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only",
+                 "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd",
+                 "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs",
+                 "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're",
+                 "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we",
+                 "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's",
+                 "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll",
+                 "you're", "you've", "your", "yours", "yourself", "yourselves"]
 
     # Sentence converted to lowercase-only
     sentence = sentence.lower()
@@ -63,11 +76,10 @@ def parse_data_from_file(filename):
 
     return sentences, labels
 
+
 sentences, labels = parse_data_from_file("./bbc-text.csv")
 
 print(f"There are {len(sentences)} sentences in the dataset.\n")
 print(f"First sentence has {len(sentences[0].split())} words (after removing stopwords).\n")
 print(f"There are {len(labels)} labels in the dataset.\n")
 print(f"The first 5 labels are {labels[:5]}")
-
-
