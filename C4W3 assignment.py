717eef501ef2f8a1064d24a58b66081040da2328
janmlew <janmikolajlewinski@gmail.com>
2023-12-04 11:52:04 +0100

    Added a part of the assignment code.

---
+++ C4W3 assignment.py
@@ -0,0 +1,96 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
+
+
+def plot_series(time, series, format="-", start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(False)
+
+
+def trend(time, slope=0):
+    return slope * time
+
+
+def seasonal_pattern(season_time):
+    """Just an arbitrary pattern, you can change it if you wish"""
+    return np.where(season_time < 0.1,
+                    np.cos(season_time * 6 * np.pi),
+                    2 / np.exp(9 * season_time))
+
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats the same pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+
+
+def noise(time, noise_level=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
+
+
+def generate_time_series():
+    # The time dimension or the x-coordinate of the time series
+    time = np.arange(4 * 365 + 1, dtype="float32")
+
+    # Initial series is just a straight line with a y-intercept
+    y_intercept = 10
+    slope = 0.005
+    series = trend(time, slope) + y_intercept
+
+    # Adding seasonality
+    amplitude = 50
+    series += seasonality(time, period=365, amplitude=amplitude)
+
+    # Adding some noise
+    noise_level = 3
+    series += noise(time, noise_level, seed=51)
+
+    return time, series
+
+
+# Save all "global" variables within the G class (G stands for global)
+@dataclass
+class G:
+    TIME, SERIES = generate_time_series()
+    SPLIT_TIME = 1100
+    WINDOW_SIZE = 20
+    BATCH_SIZE = 32
+    SHUFFLE_BUFFER_SIZE = 1000
+
+
+# Plot the generated series
+plt.figure(figsize=(10, 6))
+plot_series(G.TIME, G.SERIES)
+plt.show()
+
+
+def train_val_split(time, series, time_step=G.SPLIT_TIME):
+    time_train = time[:time_step]
+    series_train = series[:time_step]
+    time_valid = time[time_step:]
+    series_valid = series[time_step:]
+
+    return time_train, series_train, time_valid, series_valid
+
+
+# Split the dataset
+time_train, series_train, time_valid, series_valid = train_val_split(G.TIME, G.SERIES)
+
+
+def windowed_dataset(series, window_size=G.WINDOW_SIZE, batch_size=G.BATCH_SIZE, shuffle_buffer=G.SHUFFLE_BUFFER_SIZE):
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+    dataset = dataset.shuffle(shuffle_buffer)
+    dataset = dataset.map(lambda window: (window[:-1], window[-1]))
+    dataset = dataset.batch(batch_size).prefetch(1)
+    return dataset
+
+
+# Apply the transformation to the training set
+dataset = windowed_dataset(series_train)
