c2b5a1d2d10ebebb08735933530127db165acabf
janmlew <janmikolajlewinski@gmail.com>
2023-12-07 07:22:30 +0100

    Added code with parse_data_from_file function definition + resulting file.

--- C4W4 assignment.py
+++ C4W4 assignment.py
@@ -18,3 +18,35 @@ def plot_series(time, series, format="-", start=0, end=None):
     plt.xlabel("Time")
     plt.ylabel("Value")
     plt.grid(True)
+
+
+def parse_data_from_file(filename):
+    times = []
+    temperatures = []
+
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)
+        for row in reader:
+            times.append(np.datetime64(row[0]))
+            temperatures.append(float(row[1]))
+
+    return times, temperatures
+
+
+# Test your function and save all "global" variables within the G class (G stands for global)
+@dataclass
+class G:
+    TEMPERATURES_CSV = "daily-min-temperatures.csv"
+    times, temperatures = parse_data_from_file(TEMPERATURES_CSV)
+    TIME = np.array(times)
+    SERIES = np.array(temperatures)
+    SPLIT_TIME = 2500
+    WINDOW_SIZE = 64
+    BATCH_SIZE = 32
+    SHUFFLE_BUFFER_SIZE = 1000
+
+
+plt.figure(figsize=(10, 6))
+plot_series(G.TIME, G.SERIES)
+plt.show()
