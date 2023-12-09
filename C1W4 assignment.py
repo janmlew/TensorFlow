05993b7981b8fa98d72fb9e65b684a8e21f2970e
janmlew <janmikolajlewinski@gmail.com>
2023-12-09 22:17:32 +0100

    Minor tweak in the model.

--- C1W4 assignment.py
+++ C1W4 assignment.py
@@ -80,7 +80,7 @@ def train_happy_sad_model(train_generator):
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(150, 150, 3)),
         tf.keras.layers.MaxPooling2D(2, 2),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(256, activation='relu'),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
 
